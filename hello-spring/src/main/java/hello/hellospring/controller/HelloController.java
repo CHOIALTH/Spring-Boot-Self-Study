4cf8329cbf5f016fa04f11eafc8155d7f62acf0c
CHOIALTH <dnjswns4545@gmail.com>
2023-01-31 20:45:41 +0900

    230131 Self-Study w KYH Lec1

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,7 +15,39 @@ public class HelloController {
     // Spring이 model이란 것을 만들어 넣어줌.
     public String hello(Model model){
         model.addAttribute("data","hello!!");
-        // IntelliJ Ultimate Ver에서는 Ctrl + 클릭 하면 해당 경로로 이동시켜준다
+    // IntelliJ Ultimate Ver에서는 Ctrl + 클릭 하면 해당 경로로 이동시켜준다
         return "hello";
+}
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name" ,name);
+        return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    /*html에 나오는 Body 태그 말하는 것 X*/
+    /*http 통신 프롴토콜의 head부, body부 중 body부에 직접 넣어주겠다  */
+    @ResponseBody
+    public String helloString(@RequestParam(name="name") String name){
+        return "hello" + name;
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        // 여기서 hello는 객체
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
