c223abcf917a2a917ee9b7a16410be98019c3bb5
CHOIALTH <dnjswns4545@gmail.com>
2023-03-25 00:18:15 +0900

    230324 Self-Study w KYH Lec

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     /* 스프링 빈을 등록하겠다*/
     @Bean
     /* 스프링이 뜰 때 @Configuration 읽고, memberService() 로직을 호출해서
@@ -15,10 +26,14 @@ public class SpringConfig {
     public MemberService memberService(){
         return new MemberService(memberRepository());
     }
+/*    @Bean
+    public MemberRepository memberRepository(){
+        *//* MemberRepository는 인터페이스고, MemoryMemberRepository가 구현체이므로*//*
+        return new MemoryMemberRepository();
+    }*/
     @Bean
     public MemberRepository memberRepository(){
-        /* MemberRepository는 인터페이스고, MemoryMemberRepository가 구현체이므로*/
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
