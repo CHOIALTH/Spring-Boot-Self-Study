beab129da051a5a5496e6b0e98f50af541ecf5db
CHOIALTH <dnjswns4545@gmail.com>
2023-04-02 07:58:16 +0900

    [feat] 순수 자바코드 및 JUnit을 기반으로 하는 주문 및 할인 기능에 대한 테스트 코드 추가

---
+++ KYH-Spring-Core-Basic/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core.order;
+
+import hello.core.member.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        // primitive 타입으로 할 경우 null을 넣을 수 없음
+        Long memberId = 1L;
+
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
